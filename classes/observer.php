e21241f00c116cda274455af9814f110e0cf2941
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-02-22 16:33:37 +1100

    MOODLE-970: Course deletion cleanup.

---
+++ classes/observer.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Banner observer.
+ *
+ * @package    local_banner
+ * @author     Nicholas Hoobin <nicholashoobin@catalyst-au.net>
+ * @copyright  Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class local_banner_observer {
+    /**
+     * Triggered via course_deleted event.
+     *
+     * @param \core\event\course_deleted $event
+     * @return bool true on success
+     */
+    public static function course_deleted(\core\event\course_deleted $event) {
+        global $DB;
+
+        $params = array(
+            'course' => $event->courseid,
+        );
+
+        // Files are removed via $contextid->delete_content().
+
+        $banner = $DB->get_records('local_banner', $params);
+        if (!empty($banner)) {
+            $DB->delete_records('local_banner', $params);
+            return true;
+        }
+    }
+}
